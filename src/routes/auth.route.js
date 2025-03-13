7f1288d40941c217f7993696ba57a4b07130c646
ntwsam <sam.nuntawat@gmail.com>
2025-03-13 07:39:33 +0700

    API authentication (session-base)

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const authController = require('../controller/auth.controller')
+const authController = require('../controllers/auth.controller')
 const rateLimiter = require('../middleware/rateLimiter')
 const passport = require('../config/passport.config')
 
