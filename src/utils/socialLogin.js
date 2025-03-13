5be400abccec6ef69303ed598b4f98fb54f1e280
ntwsam <sam.nuntawat@gmail.com>
2025-03-13 07:47:28 +0700

    Fix missing word

--- src/utils/socialLogin.js
+++ src/utils/socialLogin.js
@@ -7,7 +7,7 @@ async function processLogin(provider, profile, done) {
         const email = profile.emails[0].value;
         const username = profile.displayName || profile.username;
 
-        const [existingUser] = await pool.promise().query('SELECT * FROM User WHERE email = ?',[email]);
+        const [existingUser] = await pool.promise().query('SELECT * FROM Users WHERE email = ?',[email]);
         if (existingUser.length > 0) {
             await pool.promise().query('UPDATE Users SET provider = ?, providerId = ?, uuid = ? WHERE id = ?',[
                 provider, profile.id,uuid,existingUser[0].id
@@ -16,7 +16,7 @@ async function processLogin(provider, profile, done) {
         } else {
             const [result] = await pool.promise().query('INSERT INTO Users (provider, providerId, uuid, username, email) VALUES (?, ?, ?, ?, ?)', [
                 provider, profile.id,uuid,username,email])
-            const [newUser] = await pool.promise().query('SELECT * FROM User WHERE id = ?', [result.insertId])
+            const [newUser] = await pool.promise().query('SELECT * FROM Users WHERE id = ?', [result.insertId])
             return done(null, newUser[0]);
         }
     }
